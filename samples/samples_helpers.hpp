0e91e072921680c1f02d39d51315fd573b455f81
Unknown <ia.dev@live.com>
2018-02-24 19:05:42 +0100

    Add StopWatch helper to samples

--- samples/samples_helpers.hpp
+++ samples/samples_helpers.hpp
@@ -1,5 +1,6 @@
 
 #include <sstream>
+#include <chrono>
 
 template < class T >
 std::string toCSV(const T& obj) { 
@@ -10,3 +11,20 @@ std::string toCSV(const T& obj) {
   }
   return ss.str();
 }
+
+class StopWatch 
+{
+ public:
+  StopWatch(){};
+  virtual ~StopWatch(){};
+ public:
+  void start() {  t_begin_ = std::chrono::steady_clock::now(); }
+  void stop() {    t_end_ = std::chrono::steady_clock::now(); }
+  double get_ms() { return this->get<double, std::milli>(); }
+  template < class T, class time_t >
+  T get() { return std::chrono::duration <T, time_t> (t_end_-t_begin_).count(); }
+  
+ protected:
+  std::chrono::time_point<std::chrono::steady_clock> t_begin_, t_end_;
+  
+};
